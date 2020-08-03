9e3d0a504a9d9912e89c78eee3b91cc5300777d7
Caroline Strickland <carolinelstrickland@outlook.com>
2020-08-02 20:55:23 -0500

    color coding hour blocks

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 $("#currentDay").text(moment().format("dddd MMMM Do"));
 
 $(".time-block").each(function(){
-    console.log($(this).children())
+    let timeBlock = parseInt($(this).attr("id").split("-")[0]);
+    let currentHour = moment().hour();
 
-    $(this).addClass("present")
-})
+    console.log(timeBlock);
+
+    if (timeBlock === currentHour) {
+        $(this).addClass("present");
+    }
+
+    if (timeBlock > currentHour) {
+        $(this).addClass("future");
+    }
+
+    if (timeBlock < currentHour) {
+        $(this).addClass("past");
+    }
+});
